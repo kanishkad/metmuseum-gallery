571eef9a87304b020b392f90b0b3b677a19aacb5
Kanishka Dilshan <kanishka.dilshan@axiatadigitallabs.com>
2022-12-15 01:38:54 +0530

    Add env files

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,7 @@
+export const environment = {
+  production: true,
+  departmentsEndpoint: 'https://collectionapi.metmuseum.org/public/collection/v1/departments',
+  objectsEndpoint: 'https://collectionapi.metmuseum.org/public/collection/v1/objects',
+  searchEndpoint: 'https://collectionapi.metmuseum.org/public/collection/v1/search',
+  sliceLimit: 10
+};
