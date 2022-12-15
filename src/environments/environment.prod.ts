f9e83dbb1228e80de3d2c379a1ca9b733e67b85a
Kanishka Dilshan <kanishka.dilshan@axiatadigitallabs.com>
2022-12-16 01:54:20 +0530

    Add ngx-glide & slider view

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -4,6 +4,6 @@ export const environment = {
   objectsEndpoint: 'https://collectionapi.metmuseum.org/public/collection/v1/objects',
   searchEndpoint: 'https://collectionapi.metmuseum.org/public/collection/v1/search',
   metadataDate: '2022-11-20',
-  departmentsSliceLimit: 5,
-  objectsSliceLimit: 5
+  departmentsSliceLimit: 3,
+  objectsSliceLimit: 8
 };
