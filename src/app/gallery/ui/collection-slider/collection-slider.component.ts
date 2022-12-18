81c181f2c17ad45732ec3b9c38a962ff42f1b1a2
Kanishka Dilshan <kanishka.dilshan@axiatadigitallabs.com>
2022-12-18 14:38:23 +0530

    Collection Slider: Migrate from ngx-glide to Flickity

--- src/app/gallery/ui/collection-slider/collection-slider.component.ts
+++ src/app/gallery/ui/collection-slider/collection-slider.component.ts
@@ -1,24 +1,48 @@
-import { Component, Input, ViewChild } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Department } from "../../../interfaces/Department";
 import { BehaviorSubject } from "rxjs";
-import { NgxGlideComponent, NgxGlideModule } from "ngx-glide";
 import { ArtefactCardComponent } from "../artefact-card/artefact-card.component";
 import { Artefact } from "../../../interfaces/Artefact";
+
+declare const Flickity: any;
 
 @Component({
   selector: 'app-collection-slider',
   standalone: true,
-  imports: [CommonModule, NgxGlideModule, ArtefactCardComponent],
+  imports: [CommonModule, ArtefactCardComponent],
   templateUrl: './collection-slider.component.html',
   styleUrls: ['./collection-slider.component.scss']
 })
-export class CollectionSliderComponent {
+export class CollectionSliderComponent implements OnInit {
+
   @Input() department!: Department;
   @Input() artefacts!: Artefact[];
 
   @Input() totalArtefacts$!: BehaviorSubject<number>;
   @Input() isLoading$!: BehaviorSubject<boolean>;
 
-  @ViewChild('ngxGlide') ngxGlide!: NgxGlideComponent;
+  ngOnInit(): void {
+    this.isLoading$.subscribe(isLoading => {
+      if (!isLoading) {
+        setTimeout(()=>{
+          // Flickity should be initialized after template is rendered because of ngFor loop
+          // This doesn't work with AfterViewInit hook
+          this.initializeFlickity();
+        })
+      }
+    })
+  }
+
+  initializeFlickity() {
+    // Flickity JS & CSS is loaded in index.html, use it to create new instance
+    const artefactSlider = document.querySelector('.artefact-slider-' + this.department.departmentId);
+    if (artefactSlider) {
+      new Flickity( artefactSlider, {
+        imagesLoaded: true,
+        percentPosition: false,
+        contain: true
+      });
+    }
+  }
 }
